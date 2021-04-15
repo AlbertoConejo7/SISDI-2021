6ec81c2bef33e1f9f2203b650c29ea37fc6bf454
Kmontano06 <keniamontano06@gmail.com>
2021-04-14 20:58:35 -0600

    Cambios en Traslado y Expedientes Prestados

---
+++ src/main/java/com/sisdi/service/TransferServiceImp.java
@@ -0,0 +1,49 @@
+package com.sisdi.service;
+
+import com.sisdi.dao.TransferDao;
+import com.sisdi.model.Transfer;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransferServiceImp implements TransferService{
+    @Autowired
+    private TransferDao transferDao;
+
+    @Override
+    public List<Transfer> listTrasfers() {
+        return (List<Transfer>) transferDao.findAll();
+    }
+
+    @Override
+    public Transfer addTransfer(Transfer transfer) {
+        return transferDao.save(transfer);
+    }
+
+    @Override
+    public List<Transfer> listTransfersState(int state) {
+        List<Transfer> transfers=this.listTrasfers();
+        List<Transfer> list= new ArrayList();
+        for(Transfer t: transfers){
+            if(t.getSTATE()== state){
+                list.add(t);
+            }
+        }
+        return list;
+    }
+    
+    @Override
+    public Transfer searchTransfer(int INDX){
+         List<Transfer> transfers=this.listTrasfers();
+         Transfer tr= new Transfer();
+         for(Transfer t: transfers){
+            if(t.getINDX()== INDX){
+                tr=t;
+            }
+        }
+         return tr;
+    
+    }
+}
