67d8b8fc571b7cd7572e6c847ae2bf049cd346db
AZVal <AZVal@DESKTOP-2Q7CQ50>
2021-04-17 16:52:56 -0700

    método de listado de departamentos

--- src/main/java/com/sisdi/data/DepartmentData.java
+++ src/main/java/com/sisdi/data/DepartmentData.java
@@ -7,6 +7,7 @@ package com.sisdi.data;
 
 import com.sisdi.model.Department;
 import com.sisdi.service.DepartmentServiceImp;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +16,33 @@ import org.springframework.stereotype.Service;
 //Data Department
 @Service
 public class DepartmentData {
+
     @Autowired
     private DepartmentServiceImp departmentServiceImp;
-    
-    private  HashMap<Integer, Department> listDepartment;
-     
-     public void init(){
+
+    private HashMap<Integer, Department> listDepartment;
+
+    public void init() {
         listDepartment = new HashMap();
         crearLista();
     }
-     
-    public void crearLista(){ 
-        List<Department> tempDepartments=departmentServiceImp.listDepartment();
-        for(Department tD:tempDepartments){
-              listDepartment.put(tD.getId(), tD);
-          }
+
+    public void crearLista() {
+        List<Department> tempDepartments = departmentServiceImp.listDepartment();
+        for (Department tD : tempDepartments) {
+            listDepartment.put(tD.getId(), tD);
+        }
     }
-    public Department getDepartment(int i){
+
+    public Department getDepartment(int i) {
         return this.listDepartment.get(i);
+    }
+
+    public List<Department> listDepartments() {
+        List<Department> aux = new ArrayList();
+        for (Department u : listDepartment.values()) {
+            aux.add(u);
+        }
+        return aux;
     }
 }
