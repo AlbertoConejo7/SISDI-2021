16915b4b8e0f1f7805f6c84faca0950b8cd8f2b0
AZVal <AZVal@DESKTOP-2Q7CQ50>
2021-04-25 18:43:33 -0700

    Cambios en el fileLoanData por cambio en la fecha

--- src/main/java/com/sisdi/data/FileLoanData.java
+++ src/main/java/com/sisdi/data/FileLoanData.java
@@ -10,6 +10,7 @@ import com.sisdi.model.FileLoan;
 import com.sisdi.model.FileLoanSimple;
 import com.sisdi.service.ExpedienteServiceImp;
 import com.sisdi.service.FileLoanServiceImp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -21,57 +22,70 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class FileLoanData {
+
     @Autowired
     private FileLoanServiceImp fileLoanServiceImp;
-    
+
     @Autowired
     private ExpedienteServiceImp expedienteServiceImp;
-    
-    public List<FileLoanSimple> listFileLoanSimples(){
-        List<FileLoanSimple> list=new ArrayList();
-        List<FileLoan> file= fileLoanServiceImp.listarFileLoans();
-        for(FileLoan f:file){
+
+    public List<FileLoanSimple> listFileLoanSimples() {
+        List<FileLoanSimple> list = new ArrayList();
+        List<FileLoan> file = fileLoanServiceImp.listarFileLoans();
+        for (FileLoan f : file) {
             list.add(this.fileLoanToFileLoanSimple(f));
         }
         return list;
     }
-    
-    public FileLoanSimple fileLoanToFileLoanSimple(FileLoan file){
-        FileLoanSimple aux=new FileLoanSimple();
-        Expediente exp= expedienteServiceImp.searchExpediente(file.getFILENAME());
+
+    public FileLoanSimple fileLoanToFileLoanSimple(FileLoan file) {
+        FileLoanSimple aux = new FileLoanSimple();
+        Expediente exp = expedienteServiceImp.searchExpediente(file.getFILENAME());
         aux.setId(file.getId());
         aux.setFileId(file.getFILE_ID());
         aux.setFileName(file.getFILENAME());
         aux.setObservations(file.getOBSERVATIONS());
-        aux.setDateCreate(file.getDATE_CREATE());
+        aux.setDateCreate(this.dateToString(file.getDATE_CREATE()));
         aux.setNameRequest(file.getNAME_REQUEST());
         aux.setDepartmentRequest(file.getDEPARTMENT_REQUEST());
-        if(file.getDEPARTMENT_REQUEST().equals(exp.getOWNER_DEPARTMENT())){
+        if (file.getDEPARTMENT_REQUEST().equals(exp.getOWNER_DEPARTMENT())) {
             aux.setDepartmentOther(exp.getRECEIVER_DEPARTMENT());
-        }else{
+        } else {
             aux.setDepartmentOther(exp.getOWNER_DEPARTMENT());
         }
         aux.setYear(this.getYear(exp.getDATE_CREATE()));
         aux.setOfiicesAmount(exp.getOFFICE_AMOUNT());
         return aux;
     }
-    
-    public FileLoan fileLoanSimpleToFileLoan(FileLoanSimple file){
-        FileLoan aux=new FileLoan();
-        
+
+    public String dateToString(Date fecha) {
+        SimpleDateFormat limFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String limit = limFormat.format(fecha);
+        return limit;
+    }
+
+    public Date stringToDate(String fecha) throws ParseException {
+        Date dateLimit = new SimpleDateFormat("dd/MM/yyyy").parse(fecha);
+        return dateLimit;
+    }
+
+    public FileLoan fileLoanSimpleToFileLoan(FileLoanSimple file) throws ParseException {
+        FileLoan aux = new FileLoan();
+
         aux.setId(file.getId());
         aux.setFILE_ID(file.getFileId());
         aux.setFILENAME(file.getFileName());
         aux.setOBSERVATIONS(file.getObservations());
-        aux.setDATE_CREATE(file.getDateCreate());
+        aux.setDATE_CREATE(this.stringToDate(file.getDateCreate()));
         aux.setNAME_REQUEST(file.getNameRequest());
         aux.setDEPARTMENT_REQUEST(file.getDepartmentRequest());
         return aux;
     }
-    private String getYear(Date date){
+
+    private String getYear(Date date) {
         SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
         String year = getYearFormat.format(date);
         return year;
     }
-    
+
 }
