c7607151b78b1b06989290c0663ed03d0940dc25
Kmontano06 <keniamontano06@gmail.com>
2021-05-02 16:59:10 -0600

    Cambios en Titulo, Departamento Receptor y Otros Documentos AddOffice

---
+++ src/main/java/com/sisdi/model/OtherDocs.java
@@ -0,0 +1,22 @@
+package com.sisdi.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "t_otherdocs")
+public class OtherDocs implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+    private String OFFICE;
+    private byte[] URL;
+    private String NAME_DOC;
+    private String TYPE_DOC;
+}
