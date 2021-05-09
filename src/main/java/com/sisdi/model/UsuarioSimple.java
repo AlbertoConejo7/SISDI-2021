2f3efb176aaf1d325403005e1c52c9e0696cd288
Kmontano06 <keniamontano06@gmail.com>
2021-05-08 21:17:08 -0600

    Perfil del Usuario

---
+++ src/main/java/com/sisdi/model/UsuarioSimple.java
@@ -0,0 +1,12 @@
+package com.sisdi.model;
+
+import lombok.Data;
+
+@Data
+public class UsuarioSimple {
+    private String name;
+    private String email;
+    private String password;
+    private String department;
+    private String certificateId;    
+}
