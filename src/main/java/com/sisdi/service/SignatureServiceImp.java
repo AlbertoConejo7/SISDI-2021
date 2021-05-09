67d1825608f3ad8150cfb5903406c3e72043ca46
Kmontano06 <keniamontano06@gmail.com>
2021-05-08 21:17:34 -0600

    Perfil del Usuario

--- src/main/java/com/sisdi/service/SignatureServiceImp.java
+++ src/main/java/com/sisdi/service/SignatureServiceImp.java
@@ -20,5 +20,23 @@ public class SignatureServiceImp implements SignatureService{
     public Signature addSignature(Signature sign) {
         return signatureDao.save(sign);
     }
+
+    @Override
+    public Signature getSignatureByUser(String user) {
+        List<Signature> list = this.listSignature();
+        Signature sign=null;
+        for(Signature s:list){
+            if(s.getUSER_ID().equals(user)){
+                sign=s;
+            }
+        }
+        return sign;
+    }
+
+    @Override
+    public boolean userSignature(String user) {
+        Signature s=this.getSignatureByUser(user);
+        return s != null;
+    }
     
 }
